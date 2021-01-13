f9b707b5e2d2f98d5ecd0e910ce5f50e7f07574b
Francisco Valle <vallecesco@gmail.com>
2021-01-12 22:53:57 -0600

    Implementacion completada

--- index.php
+++ index.php
@@ -5,3 +5,5 @@ require __DIR__ . '/vendor/autoload.php';
 var_dump(App\Validate::email('frankjose00@gmail.com'));
 
 var_dump(App\Validate::url('https://platzi.com/clases/datos-php/'));
+
+var_dump(App\Validate::password('12345678'));
